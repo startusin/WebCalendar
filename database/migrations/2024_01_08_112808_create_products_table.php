ec4d468d74f8a70ce37199f92d01802290866d86
Dolaus <dsadasdasdas19@gmail.com>
2024-01-12 20:03:57 +0200

    fix

--- database/migrations/2024_01_08_112808_create_products_table.php
+++ database/migrations/2024_01_08_112808_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('calendar_id')->references('id')->on('users');
+            $table->foreignId('calendar_id')->constrained('users')->onDelete('cascade');
             $table->string('title');
             $table->string('short_description');
             $table->longText('description');
