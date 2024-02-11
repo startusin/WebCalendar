c499c16cf9fa0f0db8d10d2a280e46e8ea510a7e
Vitaliy Makovyak <thisinfoms@gmail.com>
2024-02-11 11:29:42 +0100

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,17 +2,30 @@
 
 namespace App\Console;
 
+use App\Console\Commands\FlushPendingBookings;
+use App\Console\Commands\SendBookingReminders;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        FlushPendingBookings::class,
+        SendBookingReminders::class,
+    ];
+
     /**
      * Define the application's command schedule.
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+//        $schedule->command('flush-pending-bookings')->everyMinute()->when(function () {
+//            return now()->minute % 20 == 0;
+//        });
+//
+//        $schedule->command('send-booking-reminders')->everyMinute()->when(function () {
+//            return now()->minute % 10 == 0;
+//        });
     }
 
     /**
--- resources/views/customer/calendarSettings/edit.blade.php
+++ resources/views/customer/calendarSettings/edit.blade.php
@@ -119,7 +119,7 @@
                                 <label for="languages">Excluded days</label>
                                 <select class="select2" name="excluded_days[]" id="excluded_days" multiple="multiple" style="width: 100%;">
                                     @foreach(['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'] as $day)
-                                        <option {{ in_array($day, $settings['excluded_days']) ? 'selected' : '1' }} value="{{ $day }}">{{ $day }}</option>
+                                        <option {{ in_array($day, $settings['excluded_days'] ?? []) ? 'selected' : '1' }} value="{{ $day }}">{{ $day }}</option>
                                     @endforeach
                                 </select>
                                 @error('excluded_days')
